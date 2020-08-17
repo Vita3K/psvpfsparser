550b4e4ceda15d850a5314fc1829ce17cc1fbcdc
EXtremeExploit <gdextremeexploit@gmail.com>
2020-08-17 17:08:32 -0300

    Revert "add support for wide string"
    This reverts commit fd3e4abaf5be7c6c49ec54d0f34f909e3e5677a4.

--- rif2zrif.h
+++ rif2zrif.h
@@ -3,6 +3,6 @@
 #include <string>
 #include <vector>
 
-std::string rif2zrif(std::wstring &drmlicpath);
+std::string rif2zrif(std::string &drmlicpath);
 
 std::vector<uint8_t> get_temp_klicensee(std::string &zrif);
