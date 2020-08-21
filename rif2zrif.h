808bcd09ceab71bca0eed9fa5e62e5974fcfe6dc
EXtremeExploit <gdextremeexploit@gmail.com>
2020-08-21 12:09:02 -0300

    send the stream instead of the string, tested

--- rif2zrif.h
+++ rif2zrif.h
@@ -3,6 +3,6 @@
 #include <string>
 #include <vector>
 
-std::string rif2zrif(std::string &drmlicpath);
+std::string rif2zrif(std::ifstream &binfile);
 
 std::vector<uint8_t> get_temp_klicensee(std::string &zrif);
